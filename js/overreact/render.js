ab71e29c0910b4f3b9034f52d1a7130c9da004b2
pjunstall <pjtunstall@gmail.com>
2024-03-28 17:32:04 +0000

    comment on old and new node maps side by side; moved lines putting text nodes into the map to the right place

--- js/overreact/render.js
+++ js/overreact/render.js
@@ -3,6 +3,8 @@ export const VNodeTo$NodeMap = new Map();
 
 export function render(vNode) {
   if (typeof vNode === "string") {
+    $NodeToVNodeMap.set(vNode, vNode);
+    VNodeTo$NodeMap.set(vNode, vNode);
     return document.createTextNode(vNode);
   }
   return renderElement(vNode);
@@ -22,20 +24,15 @@ function renderElement(vNode) {
     $node.append(render(child));
   }
 
+  // Original way of mapping vNode to $node and vice versa. For most purposes, we've switched to using the id attribute because that makes it easier to keep the link between elements as the page changes, but the original remains for now, as it's still used by the central event handler.
   $NodeToVNodeMap.set($node, vNode);
   VNodeTo$NodeMap.set(vNode, $node);
 
-  let id = vNode.attrs.id;
-  let $id = $node.id;
-  if (typeof vNode === "string") {
-    id = vNode;
-  }
-  if (typeof $node === "string") {
-    $id = $node;
-  }
+  const id = vNode.attrs.id;
+  const $id = $node.id;
 
   $NodeToVNodeMap.set($id, id);
-  VNodeTo$NodeMap.set(id, id);
+  VNodeTo$NodeMap.set(id, $id);
 
   return $node;
 }
